aba7a439ffdb1b69d1b1650c1472697bb8e704de
Ovoshnig <boost45644@gmail.com>
2024-11-26 00:46:50 +0700

    Implemented DoTween movement for figure choosing.

--- Assets/Scripts/FigureChooser.cs
+++ Assets/Scripts/FigureChooser.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -6,90 +7,81 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(AudioSource))]
 public class FigureChooser : MonoBehaviour
 {
-    [SerializeField] private AnimationCurve _movementCurve; 
     [SerializeField] private GameObject[] _figurePrefabs;
     [SerializeField] private AudioClip _swingClip;
-    [SerializeField] private float _offset;
-    [SerializeField] private float _duration;
+    [SerializeField] private float _offset = 1f;
+    [SerializeField] private float _duration = 0.5f;
 
     private AudioSource _audioSource;
     private PlayerInput _playerInput;
-    private int _currentIndex = 0;
-    private bool _isMoving = false;
+    private int _currentIndex;
+    private bool _isMoving;
 
     private void Awake()
     {
-        _audioSource = GetComponent<AudioSource>();
-        _audioSource.clip = _swingClip;
-
-        _playerInput = new PlayerInput();
-        _playerInput.FigureChoice.SwapLeft.performed += SwapLeft;
-        _playerInput.FigureChoice.SwapRight.performed += SwapRight;
-        _playerInput.FigureChoice.Choose.performed += Choose;
+        InitializeAudioSource();
+        InitializeInput();
     }
 
     private void OnEnable() => _playerInput.Enable();
 
     private void OnDisable() => _playerInput.Disable();
 
-    private void Start()
+    private void Start() => CreateFigureInstances();
+
+    private void InitializeAudioSource()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = _swingClip;
+    }
+
+    private void InitializeInput()
+    {
+        _playerInput = new PlayerInput();
+        _playerInput.FigureChoice.SwapLeft.performed += ctx => SwapFigure(-1).Forget();
+        _playerInput.FigureChoice.SwapRight.performed += ctx => SwapFigure(1).Forget();
+        _playerInput.FigureChoice.Choose.performed += ChooseFigure;
+    }
+
+    private void CreateFigureInstances()
     {
         for (int i = 0; i < _figurePrefabs.Length; i++)
         {
             Vector3 position = new(i * _offset, 0, 0);
-            GameObject figure = Instantiate(_figurePrefabs[i], position, Quaternion.Euler(0, 0, 0));
-            figure.transform.parent = transform;
+            GameObject figure = Instantiate(_figurePrefabs[i], position, Quaternion.identity);
+            figure.transform.SetParent(transform);
         }
     }
 
-    private void SwapLeft(InputAction.CallbackContext context)
+    private async UniTask SwapFigure(int direction)
     {
-        if (_currentIndex > 0 && !_isMoving)
-        {
-            _currentIndex--;
-            Move(sign: 1).Forget();
-            _isMoving = true;
+        if (_isMoving || !CanSwap(direction)) 
+            return;
 
-            _audioSource.Play();
-        }
-    }
+        _isMoving = true;
+        _audioSource.Play();
 
-    private void SwapRight(InputAction.CallbackContext context)
-    {
-        if (_currentIndex < _figurePrefabs.Length - 1 && !_isMoving)
-        {
-            _currentIndex++;
-            Move(sign: -1).Forget();
-            _isMoving = true;
-
-            _audioSource.Play();
-        }
-    }
-
-    private async UniTask Move(int sign)
-    {
-        float expiredTime = 0;
-        float startX = transform.position.x;
-
-        while (expiredTime < _duration)
-        {
-            float progress = expiredTime / _duration;
-            float currentX = startX + sign * _offset * _movementCurve.Evaluate(progress);
-            transform.position = new Vector3(currentX, 0, 0);
-            expiredTime += Time.deltaTime;
-
-            await UniTask.Yield();
-        }
-        transform.position = new Vector3(-_offset * _currentIndex, 0, 0);
+        _currentIndex += direction;
+        float targetPositionX = transform.position.x + direction * -_offset;
+        await transform.DOMoveX(targetPositionX, _duration)
+            .SetEase(EaseFactory.StopMotion(120, Ease.InOutSine))
+            .AsyncWaitForCompletion();
 
         _isMoving = false;
     }
 
-    private void Choose(InputAction.CallbackContext context)
+    private bool CanSwap(int direction) =>
+        (direction < 0 && _currentIndex > 0) || (direction > 0 && _currentIndex < _figurePrefabs.Length - 1);
+
+    private void ChooseFigure(InputAction.CallbackContext context)
     {
         GameObject chosenFigure = _figurePrefabs[_currentIndex];
-        CheckersVisualizer._playerFigures[0] = chosenFigure;
+        OnFigureChosen(chosenFigure);
+    }
 
+    private void OnFigureChosen(GameObject figure)
+    {
+        CheckersVisualizer._playerFigures[0] = figure;
         SceneManager.LoadScene(1);
     }
 }
